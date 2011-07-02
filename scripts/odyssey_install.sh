d32d15b57d110714eac7590b439db65cd2d44123
Mario Juric <mjuric@cfa.harvard.edu>
2011-07-02 04:12:28 -0400

    odyssey_install.sh: change default destination to dev

--- scripts/odyssey_install.sh
+++ scripts/odyssey_install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # 
 # Builds and installs the checked out version into /n/panlfs/mjuric/lsd
-# and links it to 'stable', unless told otherwise
+# and links it to 'dev', unless told otherwise
 #
 
 test -d /n/panlfs || { echo "This script can only be used on Harvard Odyssey cluster"; exit -1; }
 test -f setup.py || { echo "This script must be run from the directory that contains setup.py"; exit -1; }
 
 NAME=$(git describe)
-DEST=${1:-stable}
+DEST=${1:-dev}
 
 . ./scripts/load-odyssey-modules.sh 
 
