e3422419c0aac38c40c2735afe76631b7ece1abe
Mario Juric <mjuric@cfa.harvard.edu>
2011-07-25 13:07:34 -0400

    Bump version to v0.5.4

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ longdesc = """Large Survey Database"""
 
 args = { 
 	'name'			: "lsd",
-	'version'		: "0.5.0", # When you change this, modify __version__ in __init__.py
+	'version'		: "0.5.4", # When you change this, modify __version__ in __init__.py
 	'description'	 	: "Large Survey Database Python Module",
 	'long_description'	: longdesc,
 	'license'		: "GPLv2",
